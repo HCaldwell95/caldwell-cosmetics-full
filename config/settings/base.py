9b94a3cc8371e84aa39a7e7e096c267098d1c437
HCaldwell95 <hade_caldwell@msn.com>
2026-05-29 13:09:46 +0100

    Continued fix

--- config/settings/base.py
+++ config/settings/base.py
@@ -160,7 +160,7 @@ STATIC_URL = "/static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.StaticFilesStorage'
 
 STATICFILES_FINDERS = [
     "django.contrib.staticfiles.finders.FileSystemFinder",
