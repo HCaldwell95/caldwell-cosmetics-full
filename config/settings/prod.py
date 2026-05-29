3bf6b94b481a1f165fed8c0965f3b3f34c4ec28b
HCaldwell95 <hade_caldwell@msn.com>
2026-05-29 12:37:07 +0100

    Fix Heroku SSL proxy handling

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -3,8 +3,9 @@ from .base import *
 DEBUG = False
 
 ALLOWED_HOSTS = [
-    "yourdomain.com",
-    "www.yourdomain.com",
+    "*",
+    "caldwell-cosmetics-django-241f71271e98.herokuapp.com",
+    ".herokuapp.com",
 ]
 
 # --------------------------------------------------
@@ -15,6 +16,7 @@ SECURE_SSL_REDIRECT = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 SECURE_BROWSER_XSS_FILTER = True
 SECURE_CONTENT_TYPE_NOSNIFF = True
 
@@ -28,4 +30,5 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 # STATIC
 # --------------------------------------------------
 
-STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
