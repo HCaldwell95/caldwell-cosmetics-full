4b4b9794837ee2d442ed859055c220af4555895d
HCaldwell95 <hade_caldwell@msn.com>
2026-06-16 13:59:01 +0100

    CSP Update

--- config/settings/base.py
+++ config/settings/base.py
@@ -22,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 cloudinary.config(
     cloud_name=os.environ.get("CLOUDINARY_CLOUD_NAME"),
     api_key=os.environ.get("CLOUDINARY_API_KEY"),
-    api_secret=os.environ.get("CLOUDINARY_API_SECRET")
+    api_secret=os.environ.get("CLOUDINARY_API_SECRET"),
+    secure=True,
 )
 
 # --------------------------------------------------
